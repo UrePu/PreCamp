1fc044d36d85c28fddfcd8a4216862c2dfc85d53
UrePu <darkin31313@gmail.com>
2024-10-31 19:57:27 +0900

    timersetinterval

--- class/D-Day-counter/script.js
+++ class/D-Day-counter/script.js
@@ -14,41 +14,29 @@
         const dateFormat = `${inputYear}-${inputMonth}-${inputDate}`
         return dateFormat;
         
-        // console.log(inputYear, inputMonth, inputDate);
     };
 
     const counterMaker = function() {
-
-
         const targetDateInput = dateFormMaker();
         const nowDate = new Date();
-        // console.log(targetDateInput)
         const targetDate = new Date(targetDateInput).setHours(0,0,0,0);
         //TargetDate 가 오전 9시로 받아져있으니 .setHours(0,0,0,0)으로 해당 시간을 0시0분0초0ms로 변경해서 자정으로 세팅
-        // console.log(nowDate)
-        // console.log(targetDate)
+
         const remaining = (targetDate - nowDate) / 1000
         // 만약, remaining 이 0이라면, 타이머가 종료되었습니다. 출력
         // 수도 코드 작성해보기.. 필?요?
-        if(!isNaN(targetDate) && !isNaN(remaining)){
-            container.style.display = 'flex';
-            messageContainer.style.display = 'none';
-        }else{
-            container.style.display = 'none'
-            messageContainer.style.display = 'flex';
-        }
-        console.log(remaining)
         if(remaining <= 0) {
             messageContainer.innerHTML = '<h3>타이머가 종료되었습니다.</h3>'
+            messageContainer.style.display = 'flex'
+            container.style.display = 'none'
+            return;
         }else if(isNaN(remaining)){
             //만약, 잘못된 날짜가 들어왔다면, 유효한 시간대가 아닙니다. 출력
+            messageContainer.style.display = 'flex'
             messageContainer.innerHTML = '<h3>유효한 시간대가 아닙니다.</h3>'
+            container.style.display = 'none'
+            return;
         }
-        // const remainingDate = Math.floor(remaining / 3600 / 24); 
-        // const remainingHours = Math.floor(remaining / 3600) % 24
-        // const remainingMin = Math.floor(remaining / 60) % 60
-        // const remainingSec = Math.floor(remaining) % 60
-        //targetDate - nowDate 의 값을 분류하여 날짜 시간 분 초로 보여지게 함   
         const remainingObj = {
             remainingDate : Math.floor(remaining / 3600 / 24),
             remainingHours : Math.floor(remaining / 3600) % 24,
@@ -56,42 +44,31 @@
             remainingSec : Math.floor(remaining) % 60
         }    
 
-        // const days = document.getElementById('days')
-        // const hours = document.getElementById('hours')
-        // const min = document.getElementById('min')
-        // const sec = document.getElementById('sec')
-
         const timeKeys = Object.keys(remainingObj);
         const documentArr = ['days', 'hours', 'min', 'sec']
-
-
-        const documentObj = { 
-
-           days : document.getElementById('days'),
-           hours : document.getElementById('hours'),
-           min : document.getElementById('min'),
-           sec : document.getElementById('sec')
-        }
-
-        
-                
-        
-        for (let i = 0 ; i < timeKeys.length; i = i + 1){
-            
-            // console.log(documentObj[docKeys[i]]);
-            documentObj[docKeys[i]].textContent = remainingObj[timeKeys[i]];
-        }
         let i = 0;
-        for(let key in documentObj){
-            documentObj[key].textContent = remainingObj[timeKeys[i]]
-            //i = i + i
-            i++;
-        }
-        
-        // documentObj[days] = remainingObj,['remainingDate']
-        // documentObj[hours] = remainingObj,['remainingHours']
-        // documentObj[min] = remainingObj,['remainingMin']
-        // documentObj[sec] = remainingObj,['remainingSec']
-        // console.log(remainingDate , remainingHours, remainingMin, remainingSec);
+        for (let tag of documentArr){
+            document.getElementById(tag).innerHTML = remainingObj[timeKeys[i]]; 
+            i++
+        };
+
+    };
+
+    const intervalIdArr =[]
+    const starter = function () {
+        setClearInterval();
+        container.style.display='flex';
+        messageContainer.style.display = 'none'
+        counterMaker();
+        const intervalId = setInterval(counterMaker,1000);
+        intervalIdArr.push(intervalId);
     }
 
+    const setClearInterval = function () {
+    container.style.display = 'none'
+    messageContainer.style.display = 'flex'
+    messageContainer.innerHTML = '<h3>D-Day를 입력해 주세요.</h3>'
+        for(let i = 0; i < intervalIdArr.length; i++){
+            clearInterval(intervalIdArr[i]);
+        }
+    }
